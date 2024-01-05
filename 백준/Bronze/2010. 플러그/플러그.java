693aff399a49f59c007c776cbaa7efa95614004a
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-01-05 10:56:11 +0900

    [Bronze III] Title: 플러그, Time: 276 ms, Memory: 40984 KB -BaekjoonHub

---
+++ 백준/Bronze/2010. 플러그/플러그.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int multitap = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        if (multitap == 1) {
+            System.out.print(Integer.parseInt(br.readLine()));
+        } else {
+            for (int i = 0; i < multitap - 1; i++) {
+                sum += Integer.parseInt(br.readLine()) - 1;
+            }
+            sum += Integer.parseInt(br.readLine());
+            System.out.print(sum);
+        }
+
+    }
+
+}
