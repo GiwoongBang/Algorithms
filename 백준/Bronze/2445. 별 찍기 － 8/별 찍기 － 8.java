a90b12df6343a223590b52a09c2c019cf8d0b40d
devGirin <107822628+GiwoongBang@users.noreply.github.com>
2024-02-13 15:31:21 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 492 ms, Memory: 21324 KB -BaekjoonHub

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -11,43 +11,32 @@ public class Main {
         int numberOfStarsPerLine = 2 * N;
 
         for (int i = 1; i <= N; i++) {
-            for (int q = 0; q < i; q++) {
-                System.out.print("*");
-            }
-
-            for (int w = 0; w < numberOfStarsPerLine - (2 * i); w++) {
-                System.out.print(" ");
-            }
-
-            for (int e = 0; e < i; e++) {
-                if (e == (i - 1)) {
-                    System.out.println("*");
-                    break;
-                } else {
-                    System.out.print("*");
-                }
-            }
+            stars(numberOfStarsPerLine, i);
         }
 
         for (int j = N - 1; j >= 1; j--) {
-            for (int a = 0; a < j; a++) {
-                System.out.print("*");
-            }
-
-            for (int k = 0; k < numberOfStarsPerLine - (2 * j); k++) {
-                System.out.print(" ");
-            }
-
-            for (int t = 0; t < j; t++) {
-                if (t == (j - 1)) {
-                    System.out.println("*");
-                    break;
-                } else {
-                    System.out.print("*");
-                }
-            }
+            stars(numberOfStarsPerLine, j);
         }
 
+    }
+
+    private static void stars(int numberOfStarsPerLine, int i) {
+        for (int q = 0; q < i; q++) {
+            System.out.print("*");
+        }
+
+        for (int w = 0; w < numberOfStarsPerLine - (2 * i); w++) {
+            System.out.print(" ");
+        }
+
+        for (int e = 0; e < i; e++) {
+            if (e == (i - 1)) {
+                System.out.println("*");
+                break;
+            } else {
+                System.out.print("*");
+            }
+        }
     }
 
 }
