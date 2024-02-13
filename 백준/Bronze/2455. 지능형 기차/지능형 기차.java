b64d67b723ddcd52187b2bfba832d4d331e3fc9a
devGirin <107822628+GiwoongBang@users.noreply.github.com>
2024-02-13 15:49:34 +0900

    [Bronze III] Title: 지능형 기차, Time: 124 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int max = 0;
+        int now = 0;
+        for (int i = 0; i < 3; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int getOff = Integer.parseInt(st.nextToken());
+            int getOn = Integer.parseInt(st.nextToken());
+
+            now -= getOff;
+            if (now > max) max = now;
+
+            now += getOn;
+            if (now > max) max = now;
+        }
+
+        System.out.println(max);
+    }
+
+}
