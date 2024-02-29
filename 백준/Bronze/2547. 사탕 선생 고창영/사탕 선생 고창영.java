ac3edbc251926f74ac0d4161adbb5c456a7b9832
devGirin <107822628+GiwoongBang@users.noreply.github.com>
2024-02-29 14:32:11 +0900

    [Bronze III] Title: 사탕 선생 고창영, Time: 220 ms, Memory: 20860 KB -BaekjoonHub

---
+++ 백준/Bronze/2547. 사탕 선생 고창영/사탕 선생 고창영.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int loop = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < loop; i++) {
+            br.readLine();
+            int students = Integer.parseInt(br.readLine());
+            BigInteger candies = BigInteger.ZERO;
+            for (int k = 0; k < students; k++) {
+                candies = candies.add(BigInteger.valueOf(Long.parseLong(br.readLine())));
+            }
+
+            if (candies.mod(BigInteger.valueOf(students)) == BigInteger.ZERO) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+
+}
