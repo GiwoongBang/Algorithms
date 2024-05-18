85dd2db8497a34e50780456c7e4749316ab14136
devGirin <107822628+GiwoongBang@users.noreply.github.com>
2024-05-18 14:07:54 +0900

    [Bronze V] Title: 등록, Time: 112 ms, Memory: 13856 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("205");
+        System.out.println("gwioong1");
+
+    }
+}
