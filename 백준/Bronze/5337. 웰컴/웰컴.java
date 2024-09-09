90fc2d8cb540a68acc134517188bd2d9ae5edcc0
devGirin <107822628+GiwoongBang@users.noreply.github.com>
2024-09-09 12:44:49 +0900

    [Bronze V] Title: 웰컴, Time: 92 ms, Memory: 14040 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print(".  .   .\n");
+        System.out.print("|  | _ | _. _ ._ _  _\n");
+        System.out.print("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+
+}
