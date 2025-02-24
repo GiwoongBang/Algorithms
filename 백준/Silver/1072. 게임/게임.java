d0f618395055a6a756375ca9811afbc6f425d7d4
devGirin <107822628+GiwoongBang@users.noreply.github.com>
2025-02-25 07:40:50 +0900

    [Silver III] Title: 게임, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.java
+++ 백준/Silver/1072. 게임/게임.java
@@ -1,43 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static long X;
-    static long Y;
-    static int target;
-
     public static void main(String[] args) throws IOException {
+
+        int result = -1;
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        long X = Integer.parseInt(st.nextToken());
+        long Y = Integer.parseInt(st.nextToken());
+        int winningRate = (int) (Y * 100 / X);
 
-        X = Integer.parseInt(st.nextToken());
-        Y = Integer.parseInt(st.nextToken());
-        target = (int) ((Y * 100) / X);
+        if (winningRate >= 99) {
+            System.out.println(result);
 
-        if (target >= 99) {
-            System.out.print(-1);
             return;
         }
 
         int left = 0;
         int right = 1000000000;
-        int result = -1;
 
         while (left <= right) {
             int mid = (left + right) / 2;
-            int Z = (int) (((Y + mid) * 100) / (X + mid));
+            int nowWinningRate = (int) (((Y + mid) * 100) / (X + mid));
 
-            if (target >= Z) {
+            if (winningRate >= nowWinningRate) {
                 result = mid + 1;
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
+
         System.out.print(result);
     }
 
